293c0534429492f92b6ba036ed8cff5f542a24d3
plamen1982 <plamen.hristov@ymail.com>
2017-07-21 13:53:37 -0400

    Func. w/ named parameters

--- lecture_3/takovata.py
+++ lecture_3/takovata.py
@@ -40,11 +40,39 @@ print(b)
 
 """ - 4 Parameters with value by default
 
-"""
-
 def bigger(param1, param2 = 23):
     if param1 > param2:
         return param1
     return  param2
 result = bigger(34)
 print(result)
+
+"""
+
+""" - 5 Named parameters as tuple
+
+"""
+
+def send_email(subject, body,  from_email, to_email, cc_email, attachments):
+    print(subject)
+    print(body)
+    print(from_email)
+    print(to_email)
+    print(cc_email)
+    print(attachments)
+
+send_email(
+
+    subject='What is your name and what it means?',
+    body = """
+    My name is Plamen Hristov, What is your name '
+and what it means? I am from Dupnitsa, Bulgaria.
+Where are you from? What is your name and what it means? \n""",
+    from_email='from_email: plamen.hristov@ymail.com',
+    to_email='to_email: paxi@mail.bg',
+    cc_email='cc_email: paxhristov@yahoo.com',
+    attachments=None
+
+)
+
+
