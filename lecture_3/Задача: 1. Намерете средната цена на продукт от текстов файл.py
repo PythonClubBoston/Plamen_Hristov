2b76d451de865223e4e4170d2b55ad1cf910cc8c
plamen1982 <plamen.hristov@ymail.com>
2017-07-23 14:31:04 -0400

    Created function extract_info_in_a_list

--- lecture_3/Задача: 1. Намерете средната цена на продукт от текстов файл.py
+++ lecture_3/Задача: 1. Намерете средната цена на продукт от текстов файл.py
@@ -12,29 +12,48 @@ catalog_full.csv
 Напишете програма, която намира средната цена от всички артикули във файла.
 
 Структурата на CSV файловете е следната:
-
-каталожен номер
-име на продукта
-цветове на продукта. Ако са повече от един са разделени с /
-за какъв вид активност е предназначен артикула
-каква е групата на артикула
-за кой пол и възраст е предназначен артикула
-цена
+1. каталожен номер
+2. име на продукта
+3. цветове на продукта.
+4. Ако са повече от един са разделени с /за какъв вид
+5. активност е предназначен артикула
+6. каква е групата на артикула за
+7. кой пол и възраст е предназначен артикула
+8. цена
 Разделителят на данните е , (запетая), а десетичният знак е . (точка)
-
 
 """
 
-products = ['10117,GRAND TOUR II,RUNNINWHT/NEW NAVY,TENNIS,SHOES,Men,691.99', 10120, 'GRAND TOUR II W,RUNNINWHT/PINK.PO.,TENNIS,SHOES,Woman,283.99']
+product_catalog_number = 0
+product_name = ''
+product_color = ''
+product_group = ''
+product_activity = ''
+product_price = 0.00
+product_gender_purpose = ''
+
+def extract_info_in_a_list(path_to_info_as_string): # return list with the info for the product
+    with open(path_to_info_as_string, 'r') as sample_catalog:
+        sample_catalog_list = []
+        for line in sample_catalog:
+            #https://stackoverflow.com/questions/3849509/how-to-remove-n-from-a-list-element
+            sample_catalog_list.append(line.strip())
+        return sample_catalog_list
+
+list_of_products = extract_info_in_a_list('./catalog_sample.csv')
+print(list_of_products)
+
+products = ['10117,GRAND TOUR II,RUNNINWHT/NEW NAVY,TENNIS,SHOES,Men,691.99', 'GRAND TOUR II W,RUNNINWHT/PINK.PO.,TENNIS,SHOES,Woman,283.99']
 
 prices_of_products = [691.99, 283.99]
 
-def averagePrice(prices_of_products): # products is an list of floats
+def calculate_average_price(prices_of_products): # products is an list of floats
     total_price = 0
     number_of_products = len(prices_of_products)
     for price_product in prices_of_products:
         total_price += price_product
     return total_price/number_of_products
 
-ave = averagePrice(prices_of_products)
+ave = calculate_average_price(prices_of_products)
 print(ave)
+
