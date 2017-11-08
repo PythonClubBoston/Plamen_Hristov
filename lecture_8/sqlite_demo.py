7c3b6deabc64b63a3c194e9f904526539e6cb5dd
plamen1982 <plamen.hristov@ymail.com>
2017-11-08 15:02:28 -0500

    Created cursor and print the results from the fetchall method

--- lecture_8/sqlite_demo.py
+++ lecture_8/sqlite_demo.py
@@ -1,7 +1,10 @@
 import sqlite3
-
+from pprint import  pprint
 # Connect to the db with the path to the db file
 
-connection = sqlite3.connect('./Northwind_small.sqlite')
-
-resultset = connection.execute('select * from Employee order by BirthDate')
+with sqlite3.connect('./Northwind_small.sqlite') as connection:
+    cursor = connection.cursor()
+    cursor.execute('select * from Employee')
+    #fetchall return a list with a tuple with the values of every columns from the table
+    results = cursor.fetchall()
+    pprint(results)
